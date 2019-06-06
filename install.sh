efcccc856549e149763dc72ca0257b0797bc4b5c
Matthew Gradidge <matthew.gradidge@fundingcircle.com>
2019-06-06 11:50:08 +0100

    Ensure config is linked correctly

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Copy zsh config
-ln -sFv ./.zshrc ~/.zshrc
+ln -sFv "$(pwd)/.zshrc" ~/.zshrc
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Install neovim
 brew install neovim
 # Copy neovim config file
 mkdir ~/.config/
-ln -sFv ./nvim/init.vim ~/.config/nvim/init.vim
+ln -sFv "$(pwd)/nvim/init.vim" ~/.config/nvim/init.vim
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # install neovim dependencies
