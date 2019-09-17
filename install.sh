ec4efc50037df8d4b7f3fa6fe3f317654bd7949c
Matthew Gradidge <matthew.gradidge@fundingcircle.com>
2019-09-17 10:48:13 +0100

    Improve the install script to include more things that I have actually installed

--- install.sh
+++ install.sh
@@ -11,19 +11,25 @@ mkdir ~/.config/
 ln -sFv "$(pwd)/nvim/init.vim" ~/.config/nvim/init.vim
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# install neovim dependencies
-nvim -s vim-commands.txt
+
 # install scm breeze
 git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
 source ~/.zshrc
+
 # install leiningen
 brew install leiningen
 # Install joker
 brew install joker
+brew install clj-kondo
 # misc
 brew install rbenv
 brew install pyenv
 brew install autojump
+
+# ruby language server
+sudo gem install solargraph
+# install neovim dependencies
+nvim -s vim-commands.txt
 # Finish
 print Done!
