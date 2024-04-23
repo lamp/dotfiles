5678d617b15ee55adb645a1e5c1f1948ecbd5627
Matthew Gradidge <matt.gradidge@gmail.com>
2024-04-23 14:04:07 -0600

    Bump home-manager version

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -75,7 +75,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.11";
+  home.stateVersion = "24.05";
 
   programs.neovim = {
     withPython3 = true;
